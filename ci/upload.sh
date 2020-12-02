f6c6f97ea832979a34ed74ed9fa4d46479c7073f
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-12-01 17:40:33 -0800

    more tweaks

--- ci/upload.sh
+++ ci/upload.sh
@@ -27,19 +27,33 @@ fi
 export GV_VERSION=$(cat VERSION)
 export COLLECTION=$(cat COLLECTION)
 
+# Gitlab’s generic package publishing API only supports a specific version
+# format, so we can only upload releases
+if ! egrep -q '^\d+\.\d+\.\d+$' VERSION; then
+  printf '%s is not a release; skipping upload\n' "${GV_VERSION}" >&2
+  # FIXME: currently it is not possible to programmatically mark this job as
+  # skipped, https://gitlab.com/gitlab-org/gitlab/-/issues/16733
+  # exit 0
+fi
+
+# FIXME
+export GV_VERSION=1.0.0
+
 chmod -R o-rwx Packages
 chmod -R g-wx Packages
 chmod -R g+X Packages
 
 for i in Packages/"${COLLECTION}"/{fedora,centos}/*/{source,os/*,debug/*}; do
-  curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
+  curl --silent \
+       --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
        --upload-file "${i}" \
-                     "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/graphviz-releases/${GV_VERSION}/${i##*/}"
+                     "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/graphviz-releases/${GV_VERSION}/${i##*/}" | tee curl-log.log
 done
 
 md5sum graphviz-"${GV_VERSION}".tar.gz >graphviz-"${GV_VERSION}".tar.gz.md5
 for i in graphviz-"${GV_VERSION}".tar.gz graphviz-"${GV_VERSION}".tar.gz.md5; do
-  curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
+  curl --silent \
+       --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
        --upload-file "${i}" \
-                     "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/graphviz-releases/${GV_VERSION}/portable_source/${i}"
+                     "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/graphviz-releases/${GV_VERSION}/portable_source/${i}" | tee curl-log.log
 done
