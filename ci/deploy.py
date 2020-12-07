02d8f1212886f3a8f65006193b45ed7a572b44aa
Matthew Fernandez <matthew.fernandez@gmail.com>
2020-12-06 16:34:18 -0800

    pad Curl output better

--- ci/deploy.py
+++ ci/deploy.py
@@ -52,7 +52,7 @@ def upload(version: str, path: str, name: Optional[str] = None) -> str:
     universal_newlines=True)
   log.info('Curl response:')
   for i, line in enumerate(output.split('\n')):
-    log.info(f' {(i + 1):2}: {line}')
+    log.info(f' {(i + 1):3}: {line}')
 
   resp = output.split('\n')[-1]
   if json.loads(resp)['message'] != '201 Created':
